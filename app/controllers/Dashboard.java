45b6b875b10fe1a62b93b57112aea9a558a4b565
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-24 17:18:05 +0000

    Iterate through all stations and update latest weather

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -3,6 +3,7 @@ package controllers;
 import models.Station;
 import play.Logger;
 import play.mvc.Controller;
+import utils.Analytics;
 
 import java.util.List;
 
@@ -11,6 +12,9 @@ public class Dashboard extends Controller
   public static void index() {
     Logger.info("Rendering Dashboard");
     List<Station> stations = Station.findAll();
+    for (Station station : stations) {
+      Analytics.updateWeather(station);
+    }
     render ("dashboard.html", stations);
   }
 }
