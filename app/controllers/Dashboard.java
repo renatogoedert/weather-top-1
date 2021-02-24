6a1a7772743cc11658d1d8849400d2f1cf03d0eb
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-24 16:22:47 +0000

    Render the Readings for the first Station on the dashboard

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,12 +1,17 @@
 package controllers;
 
+import models.Station;
 import play.Logger;
 import play.mvc.Controller;
+
+import java.util.List;
 
 public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
-    render ("dashboard.html");
+    List<Station> stations = Station.findAll();
+    Station station = stations.get(0);
+    render ("dashboard.html", station);
   }
 }
