22c5cd872a535f7d89184cf4b0db86258c42eed0
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-04 14:19:33 +0000

    dashboard now loads stations for logged in user

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import models.Station;
 import play.Logger;
 import play.mvc.Controller;
@@ -11,15 +12,18 @@ public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
-    List<Station> stations = Station.findAll();
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
     render ("dashboard.html", stations);
   }
 
   public static void addStation (String name, double lat, double lng)
   {
     Logger.info("Adding a Station");
+    Member member = Accounts.getLoggedInMember();
     Station station = new Station(name, lat, lng);
-    station.save();
+    member.stations.add(station);
+    member.save();
     redirect ("/dashboard");
   }
 }
